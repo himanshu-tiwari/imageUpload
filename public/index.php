67ab5415e58bf2ea8a8f66f4fd3e8c1b7a605dfc
himanshu tiwari <htiwarih@gmail.com>
2016-08-02 00:33:37 +0530

    Done with uploading images

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application;
@@ -23,6 +25,8 @@ $app->register(new Silex\Provider\DoctrineServiceProvider, [
 
 $app->register(new AI\Providers\UploadcareProvider);
 
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider);
+
 $app->get('/', function() use($app){
 	$images = $app['db']->prepare("SELECT * FROM images");
 	$images->execute();
@@ -30,7 +34,27 @@ $app->get('/', function() use($app){
 	$images = $images->fetchAll(\PDO::FETCH_CLASS, \AI\Models\Image::class);
 
 	return $app['twig']->render('home.twig');
-});
+})->bind('home');
+
+$app->post('/upload', function(Request $request) use($app){
+	if ($request->get('file_id') === '') {
+		return $app->redirect($app['url_generator']->generate('home'));
+	}
+
+	$file = $app['uploadcare']->getfile($request->get('file_id'));
+
+	$store = $app['db']->prepare("
+		INSERT INTO images(hash, url, created_at)
+		VALUES(:hash, :url, NOW())
+	");
+
+	$store->execute([
+		'hash' => bin2hex(random_bytes(20)),
+		'url' => $file->getUrl(),
+	]);
+
+	return $app->redirect($app['url_generator']->generate('home'));
+})->bind('image.upload');
 
 $app->run();
 
